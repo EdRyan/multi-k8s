8d1024afc8f2baa30738210e08a99aa849c3e6a9
Ed Ryan <edwardjryanjr@gmail.com>
2020-07-16 18:39:06 -0400

    Try to fix submitting to redis

--- server/index.js
+++ server/index.js
@@ -57,7 +57,7 @@ app.post('/values', async (req, res) => {
         return res.status(422).send('Index too high');
     }
 
-    redisClient.hset('values', index, 'Nothing yet!');
+    redisClient.hmset('values', index, 'Nothing yet!');
     redisPublisher.publish('insert', index);
     pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
