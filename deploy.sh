bddbcf04199e97a9aa732999c3f6aa820aa15b8b
Ed Ryan <edwardjryanjr@gmail.com>
2020-08-09 21:29:48 -0400

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t edwardjryanjr/multi-client:latest -t edwardjryanjr/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t edwardjryanjr/multi-server:latest -t edwardjryanjr/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t edwardjryanjr/multi-worker:latest -t edwardjryanjr/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push edwardjryanjr/multi-client:latest
+docker push edwardjryanjr/multi-client:$SHA
+docker push edwardjryanjr/multi-server:latest
+docker push edwardjryanjr/multi-server:$SHA
+docker push edwardjryanjr/multi-worker:latest
+docker push edwardjryanjr/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=edwardjryanjr/multi-server:$SHA
+kubectl set image deployments/client-deployment server=edwardjryanjr/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=edwardjryanjr/multi-worker:$SHA
